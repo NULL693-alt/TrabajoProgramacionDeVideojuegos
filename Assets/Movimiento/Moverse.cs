913f016436322b5dfda5b3e8288b0ac69878a70e
NULL693 <luisbetopb06@gmail.com>
2026-06-07 03:37:25 -0500

    programacion

--- Assets/Movimiento/Moverse.cs
+++ Assets/Movimiento/Moverse.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class Moverse : MonoBehaviour
 {
+
+    private int vidas = 1;
+
+    private float velocidad = 4f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
+    void movimineto()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(h, 0, v) * velocidad * Time.deltaTime);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+      movimineto();   
     }
 }
